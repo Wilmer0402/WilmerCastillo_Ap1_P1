b2cc9649c3a857220b78d79309fc79b4a8caa627
Wilmer Miguel Castillo Tejada <wilmer_castillo@ucne.edu.do>
2024-09-23 20:17:10 -0400

    Nuget Instalados Carpetas Creadas ConStr Configurado

---
+++ Models/Registros.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WilmerCastillo_Ap1_P1.Models
+{
+    public class Registros
+    {
+        [Key]
+
+        public int Id { get; set; }
+        [Required]
+
+        public string Nombres { get; set; } 
+
+
+    }
+}
