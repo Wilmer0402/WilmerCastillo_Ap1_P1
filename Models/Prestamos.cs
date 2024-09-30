8d9affd8bcb8c5a14de088b2fa506b0690bbcb79
Wilmer Miguel Castillo Tejada <wilmer_castillo@ucne.edu.do>
2024-09-30 18:28:50 -0400

    Creacion del Model Prestamos

---
+++ Models/Prestamos.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WilmerCastillo_Ap1_P1.Models
+{
+    public class Prestamos
+    {
+
+        [Key]
+
+        public int prestamosId { get; set; }
+
+        [Required(ErrorMessage = "Favor Ingresar el Nombre del Deudador")]
+
+        public string Deudador {  get; set; }
+
+        [Required(ErrorMessage = "Favor Ingresar el Concepto")]
+
+        public string Concepto { get; set; }
+
+        [Required(ErrorMessage = "Favor Ingresar el Monto del Prestamo")]
+
+        public float Monto { get; set; }    
+
+
+
+    }
+}
