e40834df04e2098171d5b473a03ab83b8a6bbf69
Wilmer Miguel Castillo Tejada <wilmer_castillo@ucne.edu.do>
2024-10-10 17:27:08 -0400

    Creacion del Models Deudaroes

---
+++ Models/Deudores.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+
+namespace WilmerCastillo_Ap1_P1.Models
+{
+    public class Deudores
+    {
+
+      [Key]
+
+        public int DeudorId { get; set; }   
+
+        [Required(ErrorMessage ="Este Campo es Obligatorio")]
+        [RegularExpression(@"^[a-zA-Z\s]+$", ErrorMessage = "Solo se Puede Ingresar Letras")]
+
+        public string? Nombres { get; set; }    
+
+
+    }
+}
