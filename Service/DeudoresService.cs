7a8510df61af810132ccfbd3c7d10594d156a993
Wilmer Miguel Castillo Tejada <wilmer_castillo@ucne.edu.do>
2024-10-10 17:27:50 -0400

    Creacion del DeudoresService

---
+++ Service/DeudoresService.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SQLitePCL;
+using System.Linq.Expressions;
+using WilmerCastillo_Ap1_P1.DAL;
+using WilmerCastillo_Ap1_P1.Models;
+
+namespace WilmerCastillo_Ap1_P1.Service
+{
+    public class DeudoresService
+    {
+        public readonly Context _context;
+        public DeudoresService(Context context)
+        {
+            _context = context; 
+        }
+
+        public async Task<List<Deudores>> Listar(Expression<Func<Deudores, bool>> criterio)
+        {
+            return await _context.Deudores.AsNoTracking()
+                .Where(criterio)
+                .ToListAsync();
+        }
+    }
+}
