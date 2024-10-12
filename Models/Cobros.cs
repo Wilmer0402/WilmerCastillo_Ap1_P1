e2aa4d8d97ed5adc4d2ad2451cd9b625d81f64a5
Wilmer Miguel Castillo Tejada <wilmer_castillo@ucne.edu.do>
2024-10-11 22:04:13 -0400

    Creacion del Models Cobros.cs

---
+++ Models/Cobros.cs
@@ -0,0 +1,34 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace WilmerCastillo_Ap1_P1.Models
+{
+    public class Cobros
+    {
+
+        [Key]
+
+        public int CobrosId { get; set; }
+
+        [Required(ErrorMessage = "Favor Ingresar la Fecha")]
+
+        public DateTime Fecha {  get; set; }
+
+        [ForeignKey("Deudores")]
+
+        public int DeudorId { get; set; }
+
+        public Deudores? Deudores { get; set; }
+
+        [ForeignKey("CobroId")]
+
+        [Required(ErrorMessage = "Favor Ingresar el Valor Cabrado")]
+        [RegularExpression(@"^\d+(\.\d+)?$", ErrorMessage = "Solo se Permiten Numeros")]
+
+        public double Monto { get; set; }
+
+ 
+        public ICollection<CobrosDetalles> CobrosDetalles { get; set; } = new List<CobrosDetalles>();
+
+    }
+}
